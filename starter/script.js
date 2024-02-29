f894989b29e4d29cc06c802728d148e7dc78bafb
sana <sana.nasani3@gmail.com>
2024-02-29 19:40:43 +0300

    resettingAndFinaleEdit

--- starter/script.js
+++ starter/script.js
@@ -14,15 +14,31 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-//starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
-//the big current scores
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+//we need to declare them as global variables
+let scores, currentScore, activePlayer, playing;
+
+//initialize the game with the function
+const init = function () {
+  //starting conditions
+  //the big current scores
+  //here we assign them values
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -66,7 +82,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2.check if player score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       //Finish the Game
       playing = false;
       diceEl.classList.add('hidden');
@@ -82,3 +98,6 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+//we dont call init
+btnNew.addEventListener('click', init);
